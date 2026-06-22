c24a1bc3be1a7d31451d4c54cf1a7d959b4df5b2
BlueBaby69 <BlueBaby69@users.noreply.github.com>
2026-06-21 22:13:27 -0500

    Fix import path and dynamic route for LeadCard

--- app/leads/[id]/page.tsx
+++ app/leads/[id]/page.tsx
@@ -1,10 +1,5 @@
-import LeadCard from "../../components/LeadCard";
+import LeadCard from "../../../components/LeadCard";
 
 export default function LeadDetails({ params }) {
-  return (
-    <div>
-      <h1>Lead Details</h1>
-      <LeadCard id={params.id} />
-    </div>
-  );
+  return <LeadCard id={params.id} />;
 }
